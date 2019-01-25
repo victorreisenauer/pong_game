5fac62ba24a6102b3936b6be6c1fca806d957c5a
Victor Reisenauer <victorreisenauer99@gmail.com>
2019-01-25 22:11:07 +0100

    feat: Setup basic app and widget functions

---
+++ pong_game.py
@@ -0,0 +1,16 @@
+"""this module runs a basic pong game application"""
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+
+if __name__ == '__main__':
+    PongApp().run()
